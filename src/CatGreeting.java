c0f74c7a71cf29b59c4d08f08448fb403575aede
Kevin Michie <kmichie32@gmail.com>
2016-05-24 20:00:33 -0500

    JavaCat
    Intro Assignment to Programming Class

---
+++ src/CatGreeting.java
@@ -0,0 +1,27 @@
+/**
+ * Creates a picture of a cat that greets the user with the phrase "Hello Java
+ * Coder.
+ * 
+ * This is practicing printing out statements along with adapting to characters
+ * that are initially identified as escape characters such as \.
+ * 
+ * @author Kevin Michie (kmichie@vt.edu)
+ * @version 2016.05.24
+ */
+
+public class CatGreeting {
+	/**
+	 * Prints out Java Cat
+	 * 
+	 * @param args
+	 *            takes an array of Arguments
+	 */
+	public static void main(String[] args) {
+
+		System.out.println(" /\\_/\\     -----");
+		System.out.println("( ' ' )  / Hello \\");
+		System.out.println("(  -  ) <  Java   |");
+		System.out.println(" | | |   \\ Coder!/");
+		System.out.println("(__|__)    -----");
+	}
+}
